ccfb606f77c99d2dffc783abfbe915d3dceb593d
Thomas Bell <tom.aus@outlook.com>
2014-10-31 14:03:21 +0800

    Tried to fix the fixURL function

--- assets/js/site.js
+++ assets/js/site.js
@@ -231,7 +231,7 @@ $(function () {
         conditions: []
     });
 });
-(function(){
+$(function(){
     $("body *[href]:not(.external)").toArray().forEach(function (el) { $(el).attr("href", fixURL($(el).attr("href"))); });
     $("body *[src]:not(.external)").toArray().forEach(function (el) { $(el).attr("src", fixURL($(el).attr("src"))); });
-})();
+});
